3591ab0e5d823c76d825f6cafa3bcd95703038d1
Rocafuerte330 <jose.roman8253@gmail.com>
2025-06-14 11:51:46 -0500

    Menú 4 listo, menú 5 va por la mitad

--- Clases.py
+++ Clases.py
@@ -9,8 +9,59 @@ class Paciente():
     pass
 class DICOM():
     pass
-class IMAGENES:
-    defimagenes = os.listdir("Imagenes")
+import cv2
+
+class IMAGENES: 
+    def __init__(self, Ruta, ima):
+        self.ruta = Ruta
+        self.ima = ima
+
+    def cargar_imagen(self):
+        self.ima = cv2.imread(f"Imagenes/{self.ruta}")
+        self.ima = cv2.cvtColor(self.ima, cv2.COLOR_BGR2RGB)
+
+
+    def mostrar_imagen(self):
+        if self.ima is not None:
+            plt.imshow(self.ima)
+            plt.axis("off")
+            plt.show()
+        else:
+            print("Error: No se pudo cargar la imagen.")
+
+    def binarizar_imagen(self, kernel, binarizacion):
+        ima_gray = cv2.cvtColor(self.ima, cv2.COLOR_RGB2GRAY)
+        # == Binarización de la imagen ==
+        vmin = np.min(ima_gray)
+        vmax = np.max(ima_gray)
+        if binarizacion == 1:
+            print("BINARIO")
+            Umb,imgB=cv2.threshold(ima_gray,vmin,vmax,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+            plt.imshow(imgB, cmap="gray")
+            plt.show()
+        elif binarizacion == 2:
+            print("Binario invertido")
+            Umb,imgB=cv2.threshold(ima_gray,vmin,vmax,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+            plt.imshow(imgB, cmap="gray")
+            plt.show()
+        elif binarizacion == 3:
+            print("Truncado")
+            Umb,imgB=cv2.threshold(ima_gray,vmin,vmax,cv2.THRESH_TRUNC+cv2.THRESH_OTSU)
+            plt.imshow(imgB, cmap="gray")
+            plt.show()
+        elif binarizacion == 4:
+            print("Tozero")
+            Umb,imgB=cv2.threshold(ima_gray,vmin,vmax,cv2.THRESH_TOZERO+cv2.THRESH_OTSU)
+            plt.imshow(imgB, cmap="gray")
+            plt.show()
+        elif binarizacion == 5:
+            print("Tozero invertido")
+            Umb,imgB=cv2.threshold(ima_gray,vmin,vmax,cv2.THRESH_TOZERO_INV+cv2.THRESH_OTSU)
+            plt.imshow(imgB, cmap="gray")
+            plt.show()
+
+
+
 
     
-    
+            
--- Main.py
+++ Main.py
@@ -1,6 +1,82 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import pydicom
+import os
+from Clases import *
+
+def menu():
+    return int(input("""Seleccione una opción:
+1. Procesar archivos Dicom
+2. Ingresar paciente
+3. Transformación Geométrica de un Dicom
+4. Imgresar imagen (JPG o PNG)
+5. Binarización de imagen (JPG o PNG)
+6. Salir
+R// """))
 def main():
     archivos_DICOM_IMA = {}
     pacientes = {}
+    while True:
+        R = menu()
 
+        if R == 1:
+            pass
+        elif R == 2:
+            pass
+        elif R == 3:
+            pass
+        elif R == 4:
+            imagenes = os.listdir("Imagenes")
+            print("""Estas son las imagenes disponibles, escriba el nombre de la imagen.
+NOTA: Incluya la extención de la imagen (.jpg o .png)
+Ejemplo: imagen.png""")
+            for i in imagenes:
+                print(f"- {i}")
+            # Ruta_ima = input("R// ")
+            Ruta_ima = "rostro.jpg"
+
+            ima = IMAGENES(Ruta_ima,None)
+            ima.cargar_imagen()
+            # ima.mostrar_imagen()
+            clave = os.path.splitext(Ruta_ima)[0]
+            print(clave)
+            archivos_DICOM_IMA[clave] = ima
+            print(archivos_DICOM_IMA)
+
+        elif R == 5:
+            print("""Escoja la imagen a binarizar""")
+            imagenes = list(archivos_DICOM_IMA.keys())
+            for i in imagenes:
+                print(f"- {i}")
+            # clave = input("R// ")
+            clave = "rostro"
+            ima = archivos_DICOM_IMA.get(clave,"No está en el diccionario")
+            img = ima.ima
+            im = IMAGENES(None, img)
+            print("""Escoja el tipo de binarización que desea realizar:
+1. Binario → cv2.THRESH_BINARY
+2. Binario invertido → cv2.THRESH_BINARY_INV
+3. Truncado → cv2.THRESH_TRUNC
+4. Tozero → cv2.THRESH_TOZERO
+5. Tozero invertido → cv2.THRESH_TOZERO_INV""")
+            binarizacion = int(input("R// "))
+            print("Ingrese el tamaño del kernel:")
+            row = int(input("Filas: "))
+            col = int(input("Columnas: "))
+            kernel = (row,col)
+            im.binarizar_imagen(kernel, binarizacion)
+            im.mostrar_imagen()
+            
+#             dibujo = int(input("""Escoja un dibujo:
+# 1. Cuadrado
+# 2. Circulo
+# R// """))
+        elif R == 6:
+            print("Saliendo...")
+            break
+        else:
+            print("Opcion no valida.")
 if __name__ == "__main__":
     main()
